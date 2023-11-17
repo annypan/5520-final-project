3328da105052af092c04bb8bd5c207f62839c0f5
Anni <annipan233@gmail.com>
2023-11-17 13:59:59 -0500

    Add object type

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,3 +1,7 @@
+import Data.Map
+
+type Object = Map PrimitiveValue PrimitiveValue
+
 data PrimitiveValue
     = BoolVal Bool -- https://flow.org/en/docs/types/literals/
     | StringVal String
@@ -6,8 +10,11 @@ data PrimitiveValue
     | UndefinedVal
     | EmptyVal -- https://flow.org/en/docs/types/empty/
     | AnyVal -- https://flow.org/en/docs/types/any/
-    
+    | ObjectVal Object -- https://flow.org/en/docs/types/objects/
+    deriving (Eq, Show, Ord)
+
 data Value
     = PrimitiveVal PrimitiveValue
     | EitherVal (Either PrimitiveValue PrimitiveValue)
     | MaybeVal (Maybe PrimitiveValue) -- https://flow.org/en/docs/types/maybe/
+    deriving (Eq, Show, Ord)
