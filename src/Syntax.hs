65585c76d0089a929910a3861103b657599b21db
Anni <annipan233@gmail.com>
2023-11-17 16:56:29 -0500

    Update Syntax.hs

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,6 +1,12 @@
 import Data.Map
 
-type Object = Map PrimitiveValue PrimitiveValue
+type Object = Map Expression Value
+
+data Expression
+  = Val Value -- literal values
+  | Op1 Uop Expression -- unary operators
+  | Op2 Expression Bop Expression -- binary operators
+  deriving (Eq, Show)
 
 data PrimitiveValue
     = BoolVal Bool -- https://flow.org/en/docs/types/literals/
@@ -11,10 +17,32 @@ data PrimitiveValue
     | EmptyVal -- https://flow.org/en/docs/types/empty/
     | AnyVal -- https://flow.org/en/docs/types/any/
     | ObjectVal Object -- https://flow.org/en/docs/types/objects/
-    deriving (Eq, Show, Ord)
+    deriving (Eq, Show)
 
 data Value
     = PrimitiveVal PrimitiveValue
     | EitherVal (Either PrimitiveValue PrimitiveValue)
     | MaybeVal (Maybe PrimitiveValue) -- https://flow.org/en/docs/types/maybe/
-    deriving (Eq, Show, Ord)
+    deriving (Eq, Show)
+
+data Uop
+    = Neg
+    | Not
+    | TypeOf -- https://www.digitalocean.com/community/tutorials/javascript-unary-operators-simple-and-useful#typeof
+    deriving (Eq, Show, Enum, Bounded)
+
+data Bop
+    = Plus
+    | Minus
+    | Times
+    | Divide
+    | Modulo
+    | Eq
+    | Neq
+    | Gt
+    | Ge
+    | Lt
+    | Le
+    | Concat
+    | In -- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/in
+    deriving (Eq, Show, Enum, Bounded)
