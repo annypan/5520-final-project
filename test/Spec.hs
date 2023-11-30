325a1b58dd9efc16b48578a5b26bbb535230d9b5
Anni <annipan233@gmail.com>
2023-11-30 15:23:09 -0500

    Debug typechecker; more testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,6 +9,8 @@ import qualified Data.Map as Map
 
 import qualified Data.Set as Set
 import TypeChecker
+import State (State)
+import qualified State as S
 
 -- QC properties
 prop_roundtrip_val :: Value -> Bool
@@ -42,9 +44,42 @@ prop_type_as_maybe_type t = canBeUsedAsType (PrimitiveType t) (MaybeType t)
 prop_type_as_itself :: Type -> Bool
 prop_type_as_itself t = canBeUsedAsType t t
 
+-- AnyType matches any uop
+prop_anytype_matches_any_uop :: Uop -> Bool
+prop_anytype_matches_any_uop uop = doesUopMatchType uop (PrimitiveType AnyType)
+
+-- AnyType matches any bop with any type
+prop_anytype_matches_any_bop :: Bop -> Type -> Bool
+prop_anytype_matches_any_bop bop = doesBopMatchType bop (PrimitiveType AnyType)
+
+-- if a bop matches a type, then it also matches the type in reverse order
+prop_bop_match_is_commutative :: Bop -> Type -> Type -> Bool
+prop_bop_match_is_commutative bop t1 t2 = doesBopMatchType bop t1 t2 == doesBopMatchType bop t2 t1
+
+typecheckerAllQC :: IO ()
+typecheckerAllQC = do
+  putStrLn "prop_prim_as_composite_type"
+  QC.quickCheck prop_prim_as_composite_type
+  putStrLn "prop_all_value_match_any_type"
+  QC.quickCheck prop_all_value_match_any_type
+  putStrLn "prop_no_value_match_empty_type"
+  QC.quickCheck prop_no_value_match_empty_type
+  putStrLn "prop_all_type_match_any_type"
+  QC.quickCheck prop_all_type_match_any_type
+  putStrLn "prop_type_as_maybe_type"
+  QC.quickCheck prop_type_as_maybe_type
+  putStrLn "prop_type_as_itself"
+  QC.quickCheck prop_type_as_itself
+  putStrLn "prop_anytype_matches_any_uop"
+  QC.quickCheck prop_anytype_matches_any_uop
+  putStrLn "prop_anytype_matches_any_bop"
+  QC.quickCheck prop_anytype_matches_any_bop
+  putStrLn "prop_bop_match_is_commutative"
+  QC.quickCheck prop_bop_match_is_commutative
+
 -- unit tests
 test_unit_all :: IO Counts
-test_unit_all = runTestTT $ TestList [testDoesValueMatchPrimitiveType, testCanBeUsedAsType, testGetType]
+test_unit_all = runTestTT $ TestList [testDoesValueMatchPrimitiveType, testCanBeUsedAsType, testGetType, testDoesExpressionMatchType]
 
 testDoesValueMatchPrimitiveType :: Test
 testDoesValueMatchPrimitiveType =
@@ -70,24 +105,38 @@ testCanBeUsedAsType =
     TestList
         [
             "BoolType can be used as BoolType" ~: canBeUsedAsType (PrimitiveType BoolType) (PrimitiveType BoolType) ~?= True,
-            "ObjectType can be used as BoolType" ~: canBeUsedAsType (PrimitiveType ObjectType) (PrimitiveType BoolType) ~?= True,
             "BoolType can be used as AnyType" ~: canBeUsedAsType (PrimitiveType BoolType) (PrimitiveType AnyType) ~?= True,
-            "AnyType can be used as BoolType" ~: canBeUsedAsType (PrimitiveType AnyType) (PrimitiveType BoolType) ~?= True,
+            "NullType can be used as AnyType" ~: canBeUsedAsType (PrimitiveType NullType) (PrimitiveType AnyType) ~?= True,
             "BoolType can be used as MaybeType BoolType" ~: canBeUsedAsType (PrimitiveType BoolType) (MaybeType BoolType) ~?= True,
-            "MaybeType BoolType can NOT be used as BoolType" ~: canBeUsedAsType (MaybeType BoolType) (PrimitiveType BoolType) ~?= False
+            "NumberType can be used as UnionType [NumberType, StringType]" ~: canBeUsedAsType (PrimitiveType NumberType) (UnionType [NumberType, StringType]) ~?= True,
+            "MaybeType BoolType can NOT be used as BoolType" ~: canBeUsedAsType (MaybeType BoolType) (PrimitiveType BoolType) ~?= False,
+            "ObjecType can NOT be used as EmptyType" ~: canBeUsedAsType (PrimitiveType ObjectType) (PrimitiveType EmptyType) ~?= False,
+            "MaybeType BoolType can be used as UnionType [BoolType, UndefinedType, NullType]" ~: canBeUsedAsType (MaybeType BoolType) (UnionType [BoolType, UndefinedType, NullType]) ~?= True,
+            "UnionType [BoolType, NullType] can be used as MaybeType BoolType" ~: canBeUsedAsType (UnionType [BoolType, UndefinedType, NullType]) (MaybeType BoolType) ~?= True
         ]
 
 testGetType :: Test
 testGetType = 
     TestList
         [
-            "GetType of BoolVal True" ~: getType (BoolVal True) ~?= BoolType,
-            "GetType of StringVal \"hello\"" ~: getType (StringVal "hello") ~?= StringType,
-            "GetType of NumberVal 1" ~: getType (NumberVal 1) ~?= NumberType,
-            "GetType of ObjectVal Map.empty" ~: getType (ObjectVal Map.empty) ~?= ObjectType,
-            "GetType of UndefinedVal" ~: getType UndefinedVal ~?= UndefinedType,
-            "GetType of NullVal" ~: getType NullVal ~?= NullType
+            getType (BoolVal True) ~?= BoolType,
+            getType (StringVal "hello") ~?= StringType,
+            getType (NumberVal 1) ~?= NumberType,
+            getType (ObjectVal Map.empty) ~?= ObjectType,
+            getType UndefinedVal ~?= UndefinedType,
+            getType NullVal ~?= NullType
         ]
+
+testDoesExpressionMatchType :: Test
+testDoesExpressionMatchType = 
+    TestList
+    [
+        S.evalState (doesExpressionMatchType (Var (Name "x")) (PrimitiveType BoolType)) (Map.fromList [("x", PrimitiveType BoolType)]) ~?= Success,
+        S.evalState (doesExpressionMatchType (Var (Name "x")) (PrimitiveType BoolType)) Map.empty ~?= Unknown,
+        S.evalState (doesExpressionMatchType (Var (Name "x")) (MaybeType BoolType)) (Map.fromList [("x", PrimitiveType BoolType)]) ~?= Success,
+        S.evalState (doesExpressionMatchType (Var (Name "x")) (UnionType [BoolType, NumberType])) (Map.fromList [("x", PrimitiveType BoolType)]) ~?= Success,
+        S.evalState (doesExpressionMatchType (Var (Name "x")) (PrimitiveType BoolType)) (Map.fromList [("x", MaybeType NumberType)]) ~?= Failure
+    ]
 
 main :: IO ()
 main = do
@@ -95,18 +144,8 @@ main = do
   -- QC.quickCheck prop_roundtrip_val
   -- putStrLn "roundtrip_exp"
   -- QC.quickCheck prop_roundtrip_exp
-  putStrLn "prop_prim_as_composite_type"
-  QC.quickCheck prop_prim_as_composite_type
-  putStrLn "prop_all_value_match_any_type"
-  QC.quickCheck prop_all_value_match_any_type
-  putStrLn "prop_no_value_match_empty_type"
-  QC.quickCheck prop_no_value_match_empty_type
-  putStrLn "prop_all_type_match_any_type"
-  QC.quickCheck prop_all_type_match_any_type
-  putStrLn "prop_type_as_maybe_type"
-  QC.quickCheck prop_type_as_maybe_type
-  putStrLn "prop_type_as_itself"
-  QC.quickCheck prop_type_as_itself
+  putStrLn "\n**typechecker qc tests**\n"
+  typecheckerAllQC
   putStrLn "\n**unit tests**\n"
   _ <- test_unit_all
   return ()
