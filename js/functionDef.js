4121f308d393dcaf388f50f13973b775775fd749
chenxilll <lengch@sas.upenn.edu>
2023-12-06 13:20:30 -0500

    test for, function

---
+++ js/functionDef.js
@@ -0,0 +1,3 @@
+function f(x: number, y: number): number {
+    return x + y;
+}
--- src/ASTExamples.hs
+++ src/ASTExamples.hs
@@ -1,6 +1,7 @@
 module ASTExamples where
-import Syntax
+
 import Data.Map qualified as Map
+import Syntax
 
 -- assign.js
 wAssign :: Block
@@ -66,10 +67,11 @@ wWhile =
     [ Assign (Name "x") (Val (ObjectVal (Map.fromList [("y", NumberVal 2), ("z", BoolVal True)]))),
       While
         (Op2 (Var (Dot (Var (Name "x")) "z")) Eq (Val (BoolVal True)))
-        (Block [
-          Update (Dot (Var (Name "x")) "y") (Val (NumberVal 3)),
-          Update (Dot (Var (Name "x")) "z") (Val (BoolVal False))
-        ])
+        ( Block
+            [ Update (Dot (Var (Name "x")) "y") (Val (NumberVal 3)),
+              Update (Dot (Var (Name "x")) "z") (Val (BoolVal False))
+            ]
+        )
     ]
 
 -- whlieCondConflict.js
@@ -79,8 +81,31 @@ wWhileCondConflict =
     [ Assign (Name "x") (Val (ObjectVal (Map.fromList [("y", NumberVal 2), ("z", BoolVal True)]))),
       While
         (Op2 (Op2 (Var (Dot (Var (Name "x")) "z")) Plus (Val (NumberVal 3))) Eq (Val (NumberVal 2)))
-        (Block [
-          Update (Dot (Var (Name "x")) "y") (Val (NumberVal 3)),
-          Update (Dot (Var (Name "x")) "z") (Val (BoolVal False))
-        ])
+        ( Block
+            [ Update (Dot (Var (Name "x")) "y") (Val (NumberVal 3)),
+              Update (Dot (Var (Name "x")) "z") (Val (BoolVal False))
+            ]
+        )
+    ]
+
+wFor :: Block
+wFor =
+  Block
+    [ Assign (Name "x") (Val (NumberVal 0)),
+      Assign (Name "y") (Val (BoolVal True)),
+      Assign (Name "z") (Val (StringVal "checker")),
+      For
+        (Assign (Name "i") (Val (NumberVal 0)))
+        (Op2 (Var (Name "i")) Lt (Val (NumberVal 10)))
+        (Update (Name "i") (Op2 (Var (Name "i")) Plus (Val (NumberVal 1))))
+        (Block [Update (Name "x") (Op2 (Var (Name "x")) Plus (Val (NumberVal 1))), Update (Name "y") (Val (BoolVal False)), Update (Name "z") (Val (StringVal "checker"))])
+    ]
+
+wFunctionDef :: Block
+wFunctionDef =
+  Block
+    [ FunctionDef
+        "f"
+        (FunctionType [NumberType, NumberType] NumberType)
+        (Block [Return (Op2 (Var (Name "x")) Plus (Var (Name "y")))])
     ]
